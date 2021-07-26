38700b3c8ab8fba21f469a465f2d0899dc0da062
Saran Chakravarthi <chakravarthisaran7@gmail.com>
2021-07-26 19:02:08 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./styles.css"
+
+const emojiDB = {
+    "🏤": "Post Office",
+    "🏬": "Department Store",
+    "🏠": "House",
+    "🏦": "Bank",
+    "🏝️": "Desert Island"
+}
+
+var emojiList = Object.keys(emojiDB);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [meaning, setMeaning] = useState("");
+
+    function onEmojiClick(emoji) {
+        setMeaning(emojiDB[emoji]);
+        console.log("meaning...", emojiDB[emoji])
+        console.log(emoji)
+    }
+
+    function emojiInputHandler(event) {
+        var userInput = event.target.value;
+
+        var meaning = emojiDB[userInput];
+
+        if (meaning === undefined) {
+            meaning = "we don't have this in our database";
+        }
+        setMeaning(meaning);
+    }
+
+    return (
+        <div >
+            <h1>Know your emoji</h1>
+
+            <h3>Paste the emoji below</h3>
+
+            <div style={{marginBottom:"3rem", marginTop:"3rem" }} >
+                <input size="50" style={{height:"2rem"}} onChange={emojiInputHandler} />
+            </div>
+
+            <h2>{meaning}</h2>
+
+            {
+                emojiList.map(emoji => {
+                    return <span onClick={() => onEmojiClick(emoji)} key={emoji} style={{ cursor: "pointer", fontSize: "2rem", padding: "2rem" }} >
+                        {emoji}
+                    </span>
+                })
+            }
+        </div>
+    )
 }
 
 export default App;
